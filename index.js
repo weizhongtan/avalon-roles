bec8c3d355c316bb8472d09369a1a2824e3dd2e7
wztan <wztan@hotmail.co.uk>
2017-01-08 19:04:50 +0000

    fixed server removing incorect sockets

--- index.js
+++ index.js
@@ -11,91 +11,89 @@ var players = [];
 app.use(express.static('public'));
 
 var AddPlayer = function(socket, data) {
-    /*  character name
-    good / bad
-    array containing names of characters i can see
-    string containing what i see them as */
-    var AvalonChar = function (character, goodOrBad, whoCanISee, howISeeThem) {
-    	return function(name, id) {
-    		this.name = name;
-    		this.id = id;
-    		this.character = character;
-    		this.whatIsMyFaction = goodOrBad;
-    		this.whoIs = function(person) {
-    			var known = false;
-    			whoCanISee.forEach(function(dude) {
-    				if (person.character == dude) known = true;
-    			});
-    			return (known) ? person.name + " is " + howISeeThem + ".": person.name + " is Unknown.";
-    		}
-    	}
-    }
-    
-    // Avalon characters
-    var Avalon = {};
-    Avalon.Merlin = AvalonChar("Merlin", "good", ["BadGuy", "Morgana"], "Bad");
-    Avalon.Percival = AvalonChar("Percival", "good", ["Merlin", "Morgana"], "Merlin or Morgana");
-    Avalon.GoodGuy = AvalonChar("GoodGuy", "good", [], "");
-    Avalon.BadGuy = AvalonChar("BadGuy", "bad", ["BadGuy", "Morgana"], "Bad too");
-    Avalon.Morgana = AvalonChar("Morgana", "bad", ["BadGuy"], "Bad too");
+  /*  character name
+  good / bad
+  array containing names of characters i can see
+  string containing what i see them as */
+  var AvalonChar = function (character, goodOrBad, whoCanISee, howISeeThem) {
+  	return function(name, id) {
+  		this.name = name;
+  		this.id = id;
+  		this.character = character;
+  		this.whatIsMyFaction = goodOrBad;
+  		this.whoIs = function(person) {
+  			var known = false;
+  			whoCanISee.forEach(function(dude) {
+  				if (person.character == dude) known = true;
+  			});
+  			return (known) ? person.name + " is " + howISeeThem + ".": person.name + " is Unknown.";
+  		}
+  	}
+  }
 
-    socket.avalonData = new Avalon[data.character](data.name, data.id);
-    players.push(socket);
-}
+  // Avalon characters
+  var Avalon = {};
+  Avalon.Merlin = AvalonChar("Merlin", "good", ["BadGuy", "Morgana", "Oberon"], "Bad");
+  Avalon.Percival = AvalonChar("Percival", "good", ["Merlin", "Morgana"], "Merlin or Morgana");
+  Avalon.GoodGuy = AvalonChar("GoodGuy", "good", [], "");
+  Avalon.BadGuy = AvalonChar("BadGuy", "bad", ["BadGuy", "Morgana", "Mordred"], "Bad too");
+  Avalon.Morgana = AvalonChar("Morgana", "bad", ["BadGuy", "Mordred"], "Bad too");
+  Avalon.Mordred = AvalonChar("Mordred", "bad", ["BadGuy", "Morgana"], "Bad too");
+  Avalon.Oberon = AvalonChar("Oberon", "bad", [], "");
 
-
-function EmitClientInfo() {
-    for (let j = 0; j < players.length; j++) {
-    	var view = {name: players[j].name, name2: "'" + players[j].name + "'", intro: "You are " + players[j].character + " (" + players[j].whatIsMyFaction + "). This is what you know:"};
-
-	    for (let i = 0; i < players.length; i++) {
-		    if (i != j) {
-		    	view[i] = players[j].whoIs(players[i]);
-	    	}
-    	}
-    	var template = "<h1>{{name}}</h1><h2>{{intro}}</h2>";
-    	for (let k = 0; k < players.length; k++) {
-    		template += "<p>{{" + k + "}}</p>";
-    	}
-    	clients[j].emit("info", Mustache.render(template, view));
-	}
+  // add player client socket to the players list
+  socket.avalonData = new Avalon[data.character](data.name, data.id);
+  players.push(socket);
 }
 
 function UpdateClientData() {
-    players.forEach(function(socket) {
-        var d = socket.avalonData;
-        var view = {name: d.name, intro: "You are " + d.character + " (" + d.whatIsMyFaction + "). This is what you know:"};
-        
-        var i = 0;
-        players.forEach(function(otherSocket) {
-            if (otherSocket != socket) {
-                view[i] = d.whoIs(otherSocket.avalonData);
-                i++;
-            }
-        });
+  players.forEach(function(socket) {
+      var d = socket.avalonData;
+      var view = {name: d.name, intro: "You are " + d.character + " (" + d.whatIsMyFaction + "). This is what you know:"};
 
-    	var template = "<h1>{{name}}</h1><h2>{{intro}}</h2>";
-    	for (let k = 0; k < players.length; k++) {
-    		template += "<p>{{" + k + "}}</p>";
-    	}
-    	socket.emit("info", Mustache.render(template, view));
-    });
+      var i = 0;
+      players.forEach(function(otherSocket) {
+          if (otherSocket != socket) {
+              view[i] = d.whoIs(otherSocket.avalonData);
+              i++;
+          }
+      });
+
+  	var template = "<h1>{{name}}</h1><h2>{{intro}}</h2>";
+  	for (let k = 0; k < players.length; k++) {
+  		template += "<p>{{" + k + "}}</p>";
+  	}
+  	socket.emit("info", Mustache.render(template, view));
+  });
 }
 
 io.on("connection", function(socket) {
-    socket.on("join", function(data) {
-        console.log("New Player Joined: " + data.name);
-        AddPlayer(socket, data);
-        UpdateClientData();
-    });
-    
-    socket.on('disconnect', function () {
-        var playerWhoLeft = players.splice(players.indexOf(socket), 1)[0].avalonData.name;
-        console.log("Player Left: " + playerWhoLeft);
-        UpdateClientData();
-    });
+console.log("New Client On Server")
+  socket.on("join", function(data) {
+      console.log("New Player Joined: " + data.name);
+      AddPlayer(socket, data);
+      UpdateClientData();
+      logPlayerList();
+  });
+
+  socket.on('disconnect', function () {
+  if (players.indexOf(socket) !== -1) {
+      var playerWhoLeft = players.splice(players.indexOf(socket), 1)[0].avalonData.name;
+      console.log("Player Left: " + playerWhoLeft);
+      UpdateClientData();
+  } else {
+      console.log("Unidentified Player Left");
+  }
+  logPlayerList();
+  });
 });
 
+function logPlayerList() {
+  console.log("Current Player List: \n " + players.map(function(p) {
+      return p.avalonData.name
+  }));
+}
+
 http.listen(process.env.PORT, function() {
-    console.log("listening on ", process.env.PORT);
+  console.log("listening on ", process.env.PORT);
 });
