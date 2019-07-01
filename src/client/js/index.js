aefb5b8b21f5bbacbd759a7e09c9fab686743a86
weizhongtan <me@weizhongtan.com>
2019-07-01 17:27:24 +0100

    revert to node 10

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, withRouter } from 'react-router-dom';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'regenerator-runtime/runtime';
-
 import App from './components/App';
 
 const AppWithRouter = withRouter(App);
