44cbbbdaed4509cfcaed4ec289892c7c554236d5
weizhongtan <me@weizhongtan.com>
2019-07-01 11:05:24 +0100

    rename files

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, withRouter } from 'react-router-dom';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'regenerator-runtime/runtime';
 
 import App from './components/App';
